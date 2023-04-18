dcc3230fbb1fc257955504d73a9271aca088a576
Miika Alikirri <nykseli@protonmail.com>
2023-04-18 18:27:52 +0300

    Print a pride duck

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 mod colored_string;
+use std::fs::read_to_string;
+
 use colored_string::{print_string, Color};
 
 fn main() {
+    let colors = vec![
+        Color::new(0x00, 0x00, 0x00),
+        Color::new(0xA3, 0xA3, 0xA3),
+        Color::new(0xFF, 0xFF, 0xFF),
+        Color::new(0x80, 0x00, 0x80),
+    ];
+
+    let duck = read_to_string("duck.txt").unwrap();
+    for (idx, line) in duck.lines().enumerate() {
+        let color = &colors[idx % colors.len()];
+        print_string(line, &color);
+    }
+
     let color = Color::new(0x80, 0x00, 0x80);
     print_string("Quack!", &color)
 }
